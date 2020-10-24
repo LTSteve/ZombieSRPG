fcfe99171f62a3391c7cc58886549b475d7ea27b
LTSteve <Steth1000@gmail.com>
2020-10-24 00:12:47 -0400

    renamed field of view effect

--- Assets/Scripts/Effects/FieldOfViewEffect.cs
+++ Assets/Scripts/Effects/FieldOfViewEffect.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FieldOfView : MonoBehaviour
+public class FieldOfViewEffect : MonoBehaviour
 {
     public MeshFilter Mesh_Filter;
 
